433237996abfa2c012be0b0a8b107f4077b096d8
yhc <2580577036@qq.com>
2024-08-20 09:07:21 +0800

    feat:home页面card内容展示

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,36 @@
+import { Col, Row, Card, Table } from "antd";
+import "./home.css";
+const userImg = require("../../assets/images/user.png")
 export default function Home() {
   return (
-    <div>
-      这是home
-    </div>
-  )
+    <Row className="home">
+      <Col span={8}>
+        <Card hoverable>
+          <div className="user">
+            <img src={userImg} />
+            <div className="userinfo">
+              <p className="name">Admin</p>
+              <p className="access">超级管理员</p>
+            </div>
+          </div>
+          <div className="login-info">
+            <p>
+              上次登录时间：<span>2021-7-19</span>
+            </p>
+            <p>
+              上次登录地点：<span>武汉</span>
+            </p>
+          </div>
+        </Card>
+        <Card style={{ marginTop: "20px" }} hoverable>
+          <Table
+            rowKey={"name"}
+            // columns={columns}
+            // dataSource={tableData}
+            // pagination={false}
+          />
+        </Card>
+      </Col>
+    </Row>
+  );
 }
