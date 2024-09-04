2a638e6054f07345e48eec091d9bcebbee3578c6
yhc <2580577036@qq.com>
2024-09-04 09:30:45 +0800

    feat:user page  data display

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,26 +1,85 @@
 import React, { useState, useEffect } from 'react';
-import {Button, Form, Input} from 'antd';
+import {Button, Form, Input, Table, Popconfirm} from 'antd';
 import './user.css';
 import { getUser } from '../../api';
 
 export default function User() {
+  const [listData, setListData] = useState({ // 用于获取搜索框数据以便后续进行数据过滤搜索
+    name: ''
+  });
+
+  const [tableData, setTableData] = useState([]) // 需要展示在页面的用户数据
+
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'name'
+    },
+    {
+      title: '年龄',
+      dataIndex: 'age'
+    },
+    {
+      title: '性别',
+      dataIndex: 'sex',
+      render: (val) => {
+        return val ? '女' : '男'
+      }
+    },
+    {
+      title: '出生日期',
+      dataIndex: 'birth'
+    },
+    {
+      title: '地址',
+      dataIndex: 'addr'
+    },
+    {
+      title: '操作',
+      render: (rowData) => {
+        return (
+          <div className="flex-box">
+            <Button style={{marginRight: '5px'}} onClick={() => handleClick('edit', rowData)}>编辑</Button>
+            <Popconfirm
+              title="提示"
+              description="此操作将删除该用户, 是否继续?"
+              okText="确认"
+              cancelText="取消"
+              onConfirm={() => handleDelete(rowData)}
+            >
+              <Button type="primary" danger>删除</Button>
+            </Popconfirm>
+          </div>
+        )
+      }
+    }
+  ]
+
   const handleClick = () => {
     console.log('新增');
   }
 
   const handleSearch = (e) => {
-    console.log(e);
+    console.log(e, '这是获取的搜索框数据');
+    setListData({
+      name: e.keyword
+    })
   }
 
-  const getUserData = () => {
-    getUser().then((res) => {
-      console.log(res, '这是获取的res的数据');
+  const handleDelete = () => {
+    
+  }
+
+  const getUserData = (listData) => {
+    getUser(listData).then(({data}) => {
+      console.log(data.list, 'data');
+      setTableData(data.list);
     })
   }
 
   useEffect(() => {
-    getUserData()
-  }, []);
+    getUserData(listData);
+  }, [listData]);
 
 
   return (
@@ -42,6 +101,11 @@ export default function User() {
           </Form.Item>
         </Form>
       </div>
+      <Table
+        columns={columns}
+        dataSource={tableData}
+        rowKey={"id"}
+      />
     </div>
   )
 }
