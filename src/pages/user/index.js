b611cfdc75e343c51bc8db310ba0186f0c7a3067
yhc <2580577036@qq.com>
2024-09-04 10:08:01 +0800

    feat:modal page display

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,45 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import {Button, Form, Input, Table, Popconfirm} from 'antd';
-import './user.css';
-import { getUser } from '../../api';
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Form,
+  Input,
+  Table,
+  Modal,
+  Select,
+  DatePicker,
+  InputNumber,
+  Popconfirm
+} from 'antd'
+import "./user.css";
+import { getUser } from "../../api";
 
 export default function User() {
-  const [listData, setListData] = useState({ // 用于获取搜索框数据以便后续进行数据过滤搜索
-    name: ''
+  const [listData, setListData] = useState({
+    // 用于获取搜索框数据以便后续进行数据过滤搜索
+    name: "",
   });
 
-  const [tableData, setTableData] = useState([]) // 需要展示在页面的用户数据
+  const [tableData, setTableData] = useState([]); // 需要展示在页面的用户数据
 
+  // 0（新增）1（编辑）// 用于控制模态框
+  const [modalType, setModalType] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOk = () => {}
+
+  // 表格结构配置
   const columns = [
     {
-      title: '姓名',
-      dataIndex: 'name'
+      title: "姓名",
+      dataIndex: "name",
     },
     {
-      title: '年龄',
-      dataIndex: 'age'
+      title: "年龄",
+      dataIndex: "age",
     },
     {
-      title: '性别',
-      dataIndex: 'sex',
+      title: "性别",
+      dataIndex: "sex",
       render: (val) => {
-        return val ? '女' : '男'
-      }
+        return val ? "女" : "男";
+      },
     },
     {
-      title: '出生日期',
-      dataIndex: 'birth'
+      title: "出生日期",
+      dataIndex: "birth",
     },
     {
-      title: '地址',
-      dataIndex: 'addr'
+      title: "地址",
+      dataIndex: "addr",
     },
     {
-      title: '操作',
+      title: "操作",
       render: (rowData) => {
         return (
           <div className="flex-box">
-            <Button style={{marginRight: '5px'}} onClick={() => handleClick('edit', rowData)}>编辑</Button>
+            <Button
+              style={{ marginRight: "5px" }}
+              onClick={() => handleClick("edit", rowData)}
+            >
+              编辑
+            </Button>
             <Popconfirm
               title="提示"
               description="此操作将删除该用户, 是否继续?"
@@ -47,65 +70,170 @@ export default function User() {
               cancelText="取消"
               onConfirm={() => handleDelete(rowData)}
             >
-              <Button type="primary" danger>删除</Button>
+              <Button type="primary" danger>
+                删除
+              </Button>
             </Popconfirm>
           </div>
-        )
-      }
-    }
-  ]
+        );
+      },
+    },
+  ];
 
-  const handleClick = () => {
-    console.log('新增');
-  }
+  const handleClick = (type) => {
+    setIsModalOpen(true)
+    if(type === 'add') {
+      setModalType(0)
+    } else {
+      setModalType(1)
+    }
+    // console.log(type,"这是type类型");
+  };
 
   const handleSearch = (e) => {
-    console.log(e, '这是获取的搜索框数据');
+    // console.log(e, "这是获取的搜索框数据");
     setListData({
-      name: e.keyword
-    })
-  }
+      name: e.keyword,
+    });
+  };
 
-  const handleDelete = () => {
-    
-  }
+  const handleDelete = () => {};
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   const getUserData = (listData) => {
-    getUser(listData).then(({data}) => {
-      console.log(data.list, 'data');
+    getUser(listData).then(({ data }) => {
+      // console.log(data.list, "data");
       setTableData(data.list);
-    })
-  }
+    });
+  };
 
   useEffect(() => {
     getUserData(listData);
   }, [listData]);
 
-
   return (
     <div className="user">
       <div className="flex-box space-between">
-        <Button type="primary" onClick={() => handleClick('add')}>+新增</Button>
+        <Button type="primary" onClick={() => handleClick("add")}>
+          +新增
+        </Button>
         <Form
           // form={searchForm}
           layout="inline"
           onFinish={handleSearch}
         >
-          <Form.Item
-            name="keyword"
-          >
+          <Form.Item name="keyword">
             <Input placeholder="请输入用户名" />
           </Form.Item>
           <Form.Item>
-            <Button htmlType="submit" type="primary">搜索</Button>
+            <Button htmlType="submit" type="primary">
+              搜索
+            </Button>
           </Form.Item>
         </Form>
       </div>
-      <Table
-        columns={columns}
-        dataSource={tableData}
-        rowKey={"id"}
-      />
+      <Table columns={columns} dataSource={tableData} rowKey={"id"} />
+      <Modal
+        open={isModalOpen}
+        title={modalType ? '编辑用户' : '新增用户'}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okText="确定"
+        cancelText="取消"
+      >
+        <Form
+          // form={form}
+          labelCol={{
+            span: 6,
+          }}
+          wrapperCol={{
+            span: 18,
+          }}
+          labelAlign="left"
+        >
+          { modalType == 1 &&
+            <Form.Item
+              name="id"
+              hidden
+            >
+              <Input/>
+            </Form.Item>
+          }
+          <Form.Item
+            label="姓名"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: '请输入姓名',
+              },
+            ]}
+          >
+            <Input placeholder="请输入姓名" />
+          </Form.Item>
+          <Form.Item
+            label="年龄"
+            name="age"
+            rules={[
+              {
+                type: 'number',
+                message: '年龄必须是数字',
+              },
+              {
+                required: true,
+                message: '请输入年龄',
+              },
+            ]}
+          >
+            <InputNumber placeholder="请输入年龄" />
+          </Form.Item>
+          <Form.Item
+            label="性别"
+            name="sex"
+            rules={[
+              {
+                required: true,
+                message: '性别是必选项',
+              },
+            ]}
+          >
+            <Select
+              placeholder="请选择性别"
+              options={[
+                { value: 0, label: '男' },
+                { value: 1, label: '女' }
+              ]}
+            ></Select>
+          </Form.Item>
+          <Form.Item
+            label="出生日期"
+            name="birth"
+            rules={[
+              {
+                required: true,
+                message: '请选择出生日期',
+              },
+            ]}
+          >
+            <DatePicker placeholder="请选择" format="YYYY/MM/DD" />
+          </Form.Item>
+          <Form.Item
+            label="地址"
+            name="addr"
+            rules={[
+              {
+                required: true,
+                message: '请填写地址',
+              },
+            ]}
+          >
+            <Input placeholder="请填写地址" />
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
-  )
+  );
 }
